7500062eb4321f9da9a2d3ea93fd05550079e3db
omega <omega@Eric-PC.maths.tld>
2018-02-22 20:56:53 +0000

    commmit

--- scripts/wefe-netdude/install.sh
+++ scripts/wefe-netdude/install.sh
@@ -5,17 +5,19 @@ if [ "$(id -u)" != 0 ]; then echo "Re-run as root.  Exiting..."; exit; fi
 
 # Make sure values in interfaces.d will be loaded
 # Add a stanza if not.
-if [ ! "$(grep 'source-directory /etc/network/interfaces.d/*' /etc/network/interfaces ]; then
+if [ ! "$(grep 'source /etc/network/interfaces.d/*' /etc/network/interfaces ]; then
 echo "source /etc/network/interfaces.d/*" >> /etc/network/interfaces
-fi
+fi && \
 
 # Updates the default script's shell from ksh93 to sh
 # If you're like me, and like/use ksh93, comment this line out.
 # apt-get install ksh
-sed -i 's/ksh93/sh/g' ./wefe && \
+sed -i 's/ksh93/bash/g' ./wefe && \
 
 # Create the default configuration directory
-mkdir /etc/wpa && \
+if [ ! -d /etc/wpa ]; then mkdir /etc/wpa; fi && \
 
 # Move the wefe script to /usr/local/bin
 cp ./wefe /usr/local/bin && \
+
+exit 0
