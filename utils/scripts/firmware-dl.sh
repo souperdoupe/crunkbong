09864c1e9a9bba193c41e197ded9c058905ea603
souperdoupe <rh.webadmin@zoho.com>
2018-04-19 07:46:12 +0000

    commit

--- utils/scripts/firmware-dl.sh
+++ utils/scripts/firmware-dl.sh
@@ -1,17 +1,34 @@
 #!/bin/sh
 
+# Lousy download automation for proprietary firmware .debs
+
+# In theory, this script could be used to batch-download any
+# set of .debs; however, I'm keeping it focused on this very
+# specific purpose for the time being.
+
 # Proprietary firmware directory
-pfdir="/tmp/debs"
+pfdir="/freya/crunkbong/0.7/home/crunkbong/wireless-firmware"
 # Firmware list
-firm="/tmp/firmware.txt"
+firm="/tmp/firmware.list"
+# Apt-cache search package names
+# Will be grepped against the string "firmware"
+pkglist="wireless b43 intelwimax bnx"
+
 # Function to trim the fat off .deb listings
 trim(){
 sed 's/ - .*//g'
 }
 
-if [ -f $firm ]; then rm $firm; fi
-apt-cache search wireless | grep firmware | trim >> $firm
-apt-cache search b43 | trim >> $firm
+# Bleach the old firmware list
+if [ -f $firm ]; then cat /dev/null > /tmp/firm; fi
+# Aggregate a list of packages
+# Should also write something to append version numbers
+# so the next process can check for updated drivers.
+# C'est la vie.
+for i in $pkglist; do
+	apt-cache search | grep firmware | trim >> $firm
+done
+# Enter the working directory
 cd $pfdir
 
 # Loop through each hit with the keyword wireless and firmware
